cd189f666c671584b2507ba923bc26f9b458d860
barrydillan <queharambe@gmail.com>
2023-08-23 01:23:47 +0200

    feat: style footer

--- src/pages/footer/Footer.tsx
+++ src/pages/footer/Footer.tsx
@@ -1,10 +1,10 @@
 export default function Footer() {
   return (
-    <footer className='mt-10 p-6'>
-      <p>
-        Built with React, Tailwind CSS and Framer Motion.
-        Font-family: DEFAULT.
-        Inspired by @brittanychiang
+    <footer className='mt-20 p-6'>
+      <p className="text-xs text-left">
+        Built with <strong>React</strong>, <strong>Tailwind CSS</strong> and <strong>Framer Motion</strong>.
+        Font-family: <strong>Inter</strong>.
+        Inspired by <strong>@brittanychiang</strong>
       </p>
     </footer>
   )
