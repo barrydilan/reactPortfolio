ea635cc0ba430ae947ebc09ae3a8acb480efc82e
barrydillan <queharambe@gmail.com>
2023-09-25 22:33:31 +0200

    feat: change theme colors

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -8,8 +8,8 @@ export default function About() {
 			<h2 className="font-light">About Me</h2>
 			<div className="mt-6 flex flex-col items-center md:items-start gap-5">
 				<div className="sm:grow md:flex md:flex-col">
-					<div className="bg-primary rounded-full w-40 h-40 mt-6 mx-auto md:max-w-[180px] md:w-full"><img className="w-full h-full object-cover rounded-full" src="src/assets/img/photo.jpeg" alt="" /></div>
-					<ul className="mt-6 flex flex-col sm:flex-row md:flex-col flex-wrap md:flex-shrink">
+					<div className="bg-primary rounded-full w-40 h-40 mt-6 mx-auto md:max-w-[200px] md:max-h-[200px]"><img className="w-full h-full object-cover rounded-full" src="src/assets/img/photo.jpeg" alt="" /></div>
+					{/* <ul className="mt-6 flex flex-col sm:flex-row md:flex-col flex-wrap md:flex-shrink">
 						<ListItem
 							icon={diploma}
 							text="ONMU, Mechanical Engineering B.Sc., 2018 - 2022"
@@ -21,13 +21,13 @@ export default function About() {
 							icon={language}
 							text="Russian, Ukrainian - native"
 						></ListItem>
-					</ul>
+					</ul> */}
 				</div>
-				<p className="text-base font-light mt-6 sm:flex-none md:flex-auto self-end">
-					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut
-					reprehenderit aperiam pariatur dolorum laboriosam facere explicabo
-					enim ad rem reiciendis voluptatum iure, impedit ipsa sequi architecto
-					obcaecati. Aut, aliquam et.
+				<p className="text-base font-[300] mt-6 sm:flex-none md:flex-auto self-end">
+          Hi, my name is Harry! I am a <strong>Frontend Web Developer</strong> based in <strong>Hamburg</strong>, Germany.
+          My passion lies in coding, design, and technology, all geared towards creating exceptional user experiences. 
+					In 2022, I have graduated from a univercity with a Bachelor's degree in Mechanical Engineering. Since then, I've been on an exciting journey in the tech industry.
+          I speak <strong>English (C1)</strong>, and <strong>German (B1)</strong>, while <strong>Russian</strong> and <strong>Ukrainian</strong> are my native languages.
 				</p>
 			</div>
 		</section>
