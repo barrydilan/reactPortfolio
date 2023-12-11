bcfd68ec8767a3ec8f5687e54c49d9a106b59a89
barrydillan <queharambe@gmail.com>
2023-12-11 14:13:11 +0100

    fix: fix translation for gh

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
+import { initReactI18next } from 'react-i18next';
 
 i18n
   .use(Backend)
@@ -17,6 +17,9 @@ i18n
     fallbackLng: 'en',
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
+    },
+    backend: {
+      loadPath: '/reactPortfolio/locales/{{lng}}/{{ns}}.json',
     },
   });
 
