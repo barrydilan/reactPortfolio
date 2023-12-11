b39cdcd65a875c09a3f8101214a89c2dfd9c5452
barrydillan <queharambe@gmail.com>
2023-12-11 21:35:17 +0100

    fix: upd cv

--- src/shared/const.ts
+++ src/shared/const.ts
@@ -1,1 +1,1 @@
-export const CV_URL = "https://drive.google.com/file/d/1788-FlwQUsHpotmiBixhVYfruzpg6Bnr/view";
+export const CV_URL = "https://drive.google.com/file/d/1mnBJC7MESp221BxkhOPCT6kBheA9nXeP/view";
